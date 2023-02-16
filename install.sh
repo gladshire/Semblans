99f9f9843c31d5b19112121e2050f060b222f3d6
gladshire <miles.girard@gmail.com>
2023-02-16 15:43:20 -0600

    First official release (v0.0.1)

--- install.sh
+++ install.sh
@@ -11,7 +11,8 @@ mkdir external
 #========================================================================
 
 # Install boost libraries
-wget https://boostorg.jfrog.io/artifactory/main/release/1.81.0/source/boost_1_81_0.tar.gz
+echo "Installing Boost libraries ..."
+wget -q https://boostorg.jfrog.io/artifactory/main/release/1.81.0/source/boost_1_81_0.tar.gz
 tar -xf boost_1_81_0.tar.gz
 cd boost_1_81_0
 ./bootstrap.sh --prefix=../ --with-libraries=system,filesystem,regex
@@ -21,14 +22,16 @@ cd ..
 rm -rf boost_1_81_0*
 
 # Install rapidXML
-wget https://sourceforge.net/projects/rapidxml/files/rapidxml/rapidxml%201.13/rapidxml-1.13.zip
+echo "Installing rapidXML library ..."
+wget -q https://sourceforge.net/projects/rapidxml/files/rapidxml/rapidxml%201.13/rapidxml-1.13.zip
 unzip rapidxml-1.13.zip
 mv rapidxml-1.13 rapidxml
 mv rapidxml include/
 rm rapidxml-1.13.zip
 
 # Install libconfini
-wget https://github.com/madmurphy/libconfini/releases/download/1.16.4/libconfini-1.16.4-x86_64-bin.tar.xz
+echo "Installing libconfini library ..."
+wget -q https://github.com/madmurphy/libconfini/releases/download/1.16.4/libconfini-1.16.4-x86_64-bin.tar.xz
 tar -xf libconfini-1.16.4-x86_64-bin.tar.xz
 mkdir ./include/libconfini
 mv ./usr/include/* ./include/libconfini/
@@ -45,27 +48,31 @@ rm -rf ./usr/
 cd ./external/
 
 # Install NCBI sra-tools
-wget --output-document sratoolkit.tar.gz https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-ubuntu64.tar.gz
+echo "Installing SRA-Tools ..."
+wget -q --output-document sratoolkit.tar.gz https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-ubuntu64.tar.gz
 tar -xf sratoolkit.tar.gz
 mv sratoolkit.3.0.1* sratoolkit.3.0.1
 cd sratoolkit.3.0.1
-wget https://raw.githubusercontent.com/ncbi/sra-tools/master/LICENSE
+wget -q https://raw.githubusercontent.com/ncbi/sra-tools/master/LICENSE
 cd ../
 rm sratoolkit.tar.gz
 
 # Install FastQC
-wget --output-document fastqc.zip https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.9.zip
+echo "Installing FastQC ..."
+wget -q --output-document fastqc.zip https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.9.zip
 unzip fastqc.zip
 chmod 755 FastQC/fastqc
 rm fastqc.zip
 
 # Install Rcorrector
-wget --output-document rcorrector.tar.gz https://github.com/mourisl/Rcorrector/archive/refs/tags/v1.0.5.tar.gz
+echo "Installing Rcorrector ..."
+wget -q --output-document rcorrector.tar.gz https://github.com/mourisl/Rcorrector/archive/refs/tags/v1.0.5.tar.gz
 tar -xf rcorrector.tar.gz
 rm rcorrector.tar.gz
 
 # Install Trimmomatic
-wget --output-document trimmomatic.zip https://github.com/usadellab/Trimmomatic/files/5854859/Trimmomatic-0.39.zip
+echo "Installing Trimmomatic ..."
+wget -q --output-document trimmomatic.zip https://github.com/usadellab/Trimmomatic/files/5854859/Trimmomatic-0.39.zip
 unzip trimmomatic.zip
 cd Trimmomatic-0.39/adapters
 cat TruSeq*.fa > TruSeq_all.fa # combine all adapter seqs into one
@@ -73,7 +80,8 @@ cd ../../
 rm trimmomatic.zip
 
 # Install Kraken2
-wget --output-document kraken2.tar.gz https://github.com/DerrickWood/kraken2/archive/refs/tags/v2.1.2.tar.gz
+echo "Installing Kraken2 ..."
+wget -q --output-document kraken2.tar.gz https://github.com/DerrickWood/kraken2/archive/refs/tags/v2.1.2.tar.gz
 tar -xf kraken2.tar.gz
 cd kraken2-2.1.2
 ./install_kraken2.sh .
@@ -81,7 +89,8 @@ cd ../
 rm kraken2.tar.gz
 
 # Install Trinity
-wget --output-document trinity.tar.gz https://github.com/trinityrnaseq/trinityrnaseq/releases/download/Trinity-v2.15.1/trinityrnaseq-v2.15.1.FULL.tar.gz
+echo "Installing Trinity ..."
+wget -q --output-document trinity.tar.gz https://github.com/trinityrnaseq/trinityrnaseq/releases/download/Trinity-v2.15.1/trinityrnaseq-v2.15.1.FULL.tar.gz
 tar -xf trinity.tar.gz
 mv trinityrnaseq-v2.15.1 trinityrnaseq-2.15.1
 cd trinityrnaseq-2.15.1
@@ -90,12 +99,14 @@ cd ../
 rm trinity.tar.gz
 
 # Install NCBI BLAST
-wget --output-document ncbi-blast.tar.gz https://ftp.ncbi.nlm.nih.gov/blast/executables/LATEST/ncbi-blast-2.13.0+-x64-linux.tar.gz
+echo "Installing NCBI-BLAST+ ..."
+wget -q --output-document ncbi-blast.tar.gz https://ftp.ncbi.nlm.nih.gov/blast/executables/LATEST/ncbi-blast-2.13.0+-x64-linux.tar.gz
 tar -xf ncbi-blast.tar.gz
 rm ncbi-blast.tar.gz
 
 # Install Corset
-wget --output-document corset.tar.gz https://github.com/Oshlack/Corset/releases/download/version-1.09/corset-1.09-linux64.tar.gz
+echo "Installing Corset ..."
+wget -q --output-document corset.tar.gz https://github.com/Oshlack/Corset/releases/download/version-1.09/corset-1.09-linux64.tar.gz
 tar -xf corset.tar.gz
 mv corset-1.09-linux64 corset-1.09
 cd corset-1.09
@@ -105,18 +116,27 @@ cd ../
 rm corset.tar.gz
 
 # Install Salmon
-wget --output-document salmon.tar.gz https://github.com/COMBINE-lab/salmon/releases/download/v1.9.0/salmon-1.9.0_linux_x86_64.tar.gz
+echo "Installing Salmon ..."
+wget -q --output-document salmon.tar.gz https://github.com/COMBINE-lab/salmon/releases/download/v1.9.0/salmon-1.9.0_linux_x86_64.tar.gz
 tar -xf salmon.tar.gz
 mv salmon-1.9.0_linux_x86_64 salmon-1.9.0
 cd salmon-1.9.0
-wget https://raw.githubusercontent.com/COMBINE-lab/salmon/v1.9.0/LICENSE
+wget -q https://raw.githubusercontent.com/COMBINE-lab/salmon/v1.9.0/LICENSE
 cd ../
 rm salmon.tar.gz
 
 # Install TransDecoder
-wget --output-document transdecoder.tar.gz https://github.com/TransDecoder/TransDecoder/archive/refs/tags/TransDecoder-v5.7.0.tar.gz
+echo "Installing TransDecoder ..."
+wget -q --output-document transdecoder.tar.gz https://github.com/TransDecoder/TransDecoder/archive/refs/tags/TransDecoder-v5.7.0.tar.gz
 tar -xf transdecoder.tar.gz
 mv TransDecoder-TransDecoder-v5.7.0 TransDecoder-5.7.0
 rm transdecoder.tar.gz
 
+#========================================================================
+#///////////////////   BUILD PAANDO FROM SOURCE    \\\\\\\\\\\\\\\\\\\\\\
+#========================================================================
+
+cd ../
+mkdir ./bin/
+make
 
