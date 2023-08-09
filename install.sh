97d96a324885de088678a206b63a6573cb79fbf6
gladshire <miles.girard@gmail.com>
2023-08-09 13:53:54 -0500

    Install script now checks for successful installation of dependencies

--- install.sh
+++ install.sh
@@ -91,7 +91,7 @@ mv panther_db ./data/
 rm PANTHER17.0_hmmscoring.tgz
 wget -r -q --no-parent http://data.pantherdb.org/ftp/hmm_scoring/current_release/pantherScore2.2/
 mv data.pantherdb.org/ftp/hmm_scoring/current_release/pantherScore2.2 ./external/pantherScore
-chmod +x ./external/pantherScore2.2.pl
+chmod +x ./external/pantherScore/pantherScore2.2.pl
 rm -rf data.pantherdb.org
 
 
@@ -197,10 +197,104 @@ mv ./external/TransDecoder-TransDecoder-v5.7.0 ./external/TransDecoder
 rm transdecoder.tar.gz
 
 #========================================================================
-#///////////////////   BUILD PAANDO FROM SOURCE    \\\\\\\\\\\\\\\\\\\\\\
+#//////////   TEST SUCCESSFUL INSTALLATION OF DEPENDENCIES    \\\\\\\\\\\
 #========================================================================
 
+packagesNotInstalled = ()
+
+# Check HMMER installation
+if [ ! -e "./external/hmmer/bin/hmmscan" ]; then
+	packagesNotInstalled += ("HMMER")
+fi
+
+# Check pantherScore installation
+if [ ! -e "./external/pantherScore/pantherScore2.2.pl" ]; then
+	packagesNotInstalled += ("pantherScore")
+fi
+
+# Check SRA Toolkit installation
+if ( [ ! -e "./external/sratoolkit/bin/prefetch" ] &&
+     [ -e "./external/sratoolkit/bin/fasterq-dump" ] ); then
+	packagesNotInstalled += ("SRA-Tools")
+fi
+
+# Check FastQC installation
+if [ ! -e "./external/FastQC/fastqc" ]; then
+	packagesNotInstalled += ("FastQC")
+fi
+
+# Check Rcorrector installation
+if [ ! -e "./external/Rcorrector/rcorrector" ]; then
+	packagesNotInstalled += ("Rcorrector")
+fi
+
+# Check Trimmomatic installation
+if [ ! -e "./external/Trimmomatic/trimmomatic-0.39.jar" ]; then
+	packagesNotInstalled += ("Trimmomatic")
+fi
+
+# Check Kraken2 installation
+if [ ! -e "./external/kraken2/kraken2" ]; then
+	packagesNotInstalled += ("Kraken2")
+fi
+
+# Check Trinity installation
+if [ ! -e "./external/trinityrnaseq/Trinity" ]; then
+	packagesNotInstalled += ("Trinity")
+fi
+
+# Check BLAST installation
+if ( [ ! -e "./external/ncbi-blast+/blastx" ] ||
+     [ ! -e "./external/ncbi-blast+/blastp" ] ); then
+	packagesNotInstalled += ("BLAST+")
+fi
+
+# Check Diamond installation
+if [ ! -e "./external/diamond/diamond" ]; then
+	packagesNotInstalled += ("Diamond")
+fi
+
+# Check Corset installation
+if [ ! -e "./external/corset/corset" ]; then
+	packagesNotInstalled += ("Corset")
+fi
+
+# Check Salmon installation
+if [ ! -e "./external/salmon/bin/salmon" ]; then
+	packagesNotInstalled += ("Salmon")
+fi
+
+# Check TransDecoder installation
+if ( [ ! -e "./external/TransDecoder/TransDecoder.LongOrfs" ] ||
+     [ ! -e "./external/TransDecoder/TransDecoder.Predict" ] ); then
+	packagesNotInstalled += ("TransDecoder")
+fi
+
+# Determine if any packages installed incorrectly
+if [ "${#packagesNotInstalled[@]}" -eq 0 ]; then
+	echo "All dependencies installed correctly."
+else
+	echo "ERROR: The following dependencies failed to install:"
+	for package in "${#packagesNotInstalled[@]}"; do
+		echo "  $package"
+	done
+	exit 1
+fi
+
+
+#========================================================================
+#//////////////////   BUILD SEMBLANS FROM SOURCE   \\\\\\\\\\\\\\\\\\\\\\
+#========================================================================
+
+echo "Now building Semblans ..."
 mkdir ./bin/
 make
 
-
+# Determine if build was successful
+if ( [ ! -e "./bin/preprocess" ] ||
+     [ ! -e "./bin/assemble" ] ||
+     [ ! -e "./bin/postprocess" ] ||
+     [ ! -e "./bin/semblans" ] ); then
+	echo ERROR: Semblans failed to build from source
+	exit 1
+fi
