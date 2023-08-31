fb4e916195f9fbc421137d4b2546a90623fd9f8b
gladshire <miles.girard@gmail.com>
2023-08-31 13:33:22 -0500

    Fixed bug in installation script

--- src/star_wrap.h
+++ src/star_wrap.h
@@ -9,7 +9,7 @@
 namespace fs = boost::filesystem;
 namespace dl = boost::dll;
 
-#define PATH_STAR std::string((dl::program_location().parent_path() / fs::path(std::string("../external/star/bin/Linux_x86_64/STAR"))).c_str())
+#define PATH_STAR std::string((dl::program_location().parent_path() / fs::path(std::string("../external/STAR/bin/Linux_x86_64/STAR"))).c_str())
 
 void star_index(std::vector<std::string> fastaFiles, std::string outDir, std::string threads,
                 bool dispOutput, std::string logFile);
