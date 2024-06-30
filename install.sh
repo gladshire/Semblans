97e4b1e2443894d7b7a3d71bf6f6d3cf4fdf07e8
gladshire <miles.girard@gmail.com>
2024-06-30 16:20:06 -0500

    Restrict install of Boost to python3

--- install.sh
+++ install.sh
@@ -59,7 +59,7 @@ if  [ ! -f ./lib/libboost_filesystem.a ] ||
 	wget -q https://boostorg.jfrog.io/artifactory/main/release/1.81.0/source/boost_1_81_0.tar.gz
 	tar -xf boost_1_81_0.tar.gz
 	cd boost_1_81_0 || return 1
-	./bootstrap.sh --prefix=../
+	./bootstrap.sh --prefix=../ --with-python=python3
 	./b2 install cxxflags="-std=c++11" link=static
 	mv LICENSE_1_0.txt ../include/boost/
 	cd ..
